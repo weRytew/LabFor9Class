75f641c3f07e21fab36eb56ee8a9e64b5bfbd83d
weRytew <138515217+weRytew@users.noreply.github.com>
2025-11-11 20:22:04 +0300

    Update workWithTime.py

--- workWithTime.py
+++ workWithTime.py
@@ -1,9 +1,10 @@
 def Hour(hourArray, userInputHour):
     if userInputHour > 12:
         userInputHour -= 12
+
     resaltStrForHour = f"{userInputHour}"
-    #resaltStrForHour += namberValuesArrayForHour[userInputHour]
-    if userInputHour == 0 or 5 <= userInput[0] <= 12:
+
+    if userInputHour == 0 or 5 <= userInputHour <= 12:
         resaltStrForHour += hourArray[0]
     elif userInputHour == 1:
         resaltStrForHour += hourArray[1]
@@ -25,7 +26,7 @@ def Minut(minutArray, userInputMinut):
 
     if minut == 1 and tenMinut != 1:
         resaltStrForMinut += minutArray[0]
-    elif str(minut) in "1 2" and tenMinut != 1:
+    elif str(minut) in "3 2 4" and tenMinut != 1:
         resaltStrForMinut += minutArray[1]
     else:
         resaltStrForMinut += minutArray[2]
@@ -39,7 +40,7 @@ def TimesofDay(userInputHour, userInputMinut, timesofDay):
         return timesofDay[4]
     return timesofDay [userInputHour // 6]
 
-def Main(userInput):
+def processingTime(userInput):
     hourArray = [" часов", " час", " часа"]
     minutArray = [" минута", " минуты", " минут"]
     timesofDay = ["ночи", "утора", "дня", "вечера", "полночь", "полдень"]
@@ -54,12 +55,16 @@ def Main(userInput):
     else:
         print(hour + " " + minut + " " + time)
 
-print("введите время с 00:00 до 23:59 в формате часы пробелы минуты(часы и минуты должны быть целыми числами/цифрами)")
-userInput = list(map(int, input().split()))
+def Main():
+    print("введите время с 00:00 до 23:59 в формате часы пробелы минуты(часы и минуты должны быть целыми числами/цифрами)")
+    userInput = list(map(int, input().split()))
 
-if 0 <= userInput[0] <= 23 and 0 <= userInput[1] <= 59:
-    Main(userInput)
-if 0 > userInput[0] or userInput[0] > 23:
-    print("Введены недопустимые данные: часы должны быть от 0 до 23")
-if 0 > userInput[1] or userInput[1] > 59:
-    print("Введены недопустимые данные: минуты должны быть от 0 до 59")
+    if 0 <= userInput[0] <= 23 and 0 <= userInput[1] <= 59:
+        processingTime(userInput)
+    if 0 > userInput[0] or userInput[0] > 23:
+        print("Введены недопустимые данные: часы должны быть от 0 до 23")
+    if 0 > userInput[1] or userInput[1] > 59:
+        print("Введены недопустимые данные: минуты должны быть от 0 до 59")
+
+if __name__ == "__main__":
+    Main()
